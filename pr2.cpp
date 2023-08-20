61ce4d225c1306d053c116582154a993945b6926
Maslovaem <142742065+Maslovaem@users.noreply.github.com>
2023-08-21 00:14:20 +0300

    Добавлено приглашение на ввод, проверка этого ввода

--- pr2.cpp
+++ pr2.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+int get_first(double*,double*, double*);
 int main(void)
 {
-    double a,b,c;
-    double D;
-    while(scanf("%lf %lf %lf",&a,&b,&c)==3)
+    double a=0.0;
+    double b=0.0;
+    double c=0.0;
+    double D=0.0;
+    printf("A quadratic equation looks like this: a*x^2 + b*x + c = 0. Enter a,b,c; 'q' - exit.\n");
+    while(get_first(&a,&b,&c)==1)
     {
         if(fabs(a-0.0)<0.0000001)
         {
@@ -37,13 +41,34 @@ int main(void)
             }
             else
             {
-                printf("x1 = %.6lf, x2 = %.6lf", ((-b)+sqrt(D))/(2*a), ((-b)-sqrt(D))/(2*a));
+                printf("x1 = %.6lf, x2 = %.6lf\n", ((-b)+sqrt(D))/(2*a), ((-b)-sqrt(D))/(2*a));
             }
         }
+        printf("Enter a,b,c; 'q' - exit.\n");
     }
     /*getchar(); //for *.exe
     getchar();
     getchar();*/
     return 0;
 }
+int get_first(double*a_p,double*b_p,double*c_p)
+{
+    int check=0;
+    while((check=scanf("%lf %lf %lf",a_p,b_p,c_p))!=3)
+    {
+        if(getchar()=='q'&& check==0 && getchar()=='\n')
+        {
+            return 0;
+        }
+        else
+        {
+            printf("Enter three numbers\n");
+            while(getchar()!='\n')
+            {
+                ;
+            }
+        }
+    }
+    return 1;
 
+}
