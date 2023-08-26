366fdf3f72cbf38426497a1cf836e275df1e5504
Maslovaem <142742065+Maslovaem@users.noreply.github.com>
2023-08-26 21:49:38 +0300

    Add files via upload

---
+++ output_analysis.h
@@ -0,0 +1,12 @@
+#ifndef OUTPUT_ANALYSIS
+#define OUTPUT_ANALYSIS
+
+/**
+Prints calculation results
+@param[in] variable for the first root
+@param[in] variable for the second root
+@param[in] amount of roots
+*/
+void output_f(double x1, double x2, int solver_return);
+
+#endif
